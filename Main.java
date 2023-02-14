a63774522a4780bf5c40ec4fbac0d50371b33630
ChemicalIzu <122799730+ChemicalIzu@users.noreply.github.com>
2023-02-14 16:18:14 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona ();
+        persona.setEdad(21);
+        persona.setNombre("Izumi");
+        persona.setTelefono("123-45-67-89");
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+    }
+}
+class Persona{
+    private int edad;
+    private String nombre;
+    private String telefono;
+    public void setEdad (int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setTelefono (String telefono){
+        this.telefono = telefono;
+    }
+    public String getTelefono(){
+        return telefono;
+    }
+}
